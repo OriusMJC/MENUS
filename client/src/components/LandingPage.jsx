e63a064b449869e0488bc143b46aab5e720a5e23
OriusMJC <matiasjesuscontreras12@hotmail.com>
2022-07-24 16:14:24 -0300

    Change

--- client/src/components/LandingPage.jsx
+++ client/src/components/LandingPage.jsx
@@ -4,7 +4,7 @@ import './Style/LandingPage.css'
 export default function LandingPage(){
     return(
         <div id='lp-container'>
-            <h3>BIENVENIDO</h3>
+            <h3>BIENVENIDO!</h3>
             <h1>MENU'S</h1>
             <Link to='/home'>
                 <button>VER MENÚ</button>
