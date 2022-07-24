b3cd0faffff74fb51131d3dd8ba67dff1460a261
OriusMJC <matiasjesuscontreras12@hotmail.com>
2022-07-24 16:25:18 -0300

    Fix searchbar and ApiKey

--- api/src/routes/recipes.js
+++ api/src/routes/recipes.js
@@ -7,7 +7,7 @@ const router = Router();
 
 
 const getAllRecipesApi = async()=>{
-    const resu = await axios(`https://api.spoonacular.com/recipes/complexSearch?apiKey=be5300dbbf044534a95cd8b9e861ed56&addRecipeInformation=true&number=100`);
+    const resu = await axios(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${API_KEY}&addRecipeInformation=true&number=100`);
     // console.log(resu.data.results[0].title)
     const recetas = resu.data.results.map(r=>{
         return {
@@ -62,7 +62,7 @@ router.get('/:idReceta',async(req,res,next)=>{
     const id = req.params.idReceta
     try {
         if(id.length < 15){
-            const resuApi = await axios(`https://api.spoonacular.com/recipes/${id}/information?apiKey=be5300dbbf044534a95cd8b9e861ed56`)
+            const resuApi = await axios(`https://api.spoonacular.com/recipes/${id}/information?apiKey=${API_KEY}`)
             if(resuApi){
                 const receta = {
                     id: resuApi.data.id,
